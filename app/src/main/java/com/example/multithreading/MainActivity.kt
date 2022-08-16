63af7b8563dabe0f5928e8dde381d0abec97ba0c
npogoncuk <npogoncuk@gmail.com>
2022-08-16 20:45:51 +0300

    App shows notification every minute. Stop button works correctly

--- app/src/main/java/com/example/multithreading/MainActivity.kt
+++ app/src/main/java/com/example/multithreading/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.multithreading
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import com.example.multithreading.databinding.ActivityMainBinding
 import java.util.concurrent.TimeUnit
+
+
+private const val CHANNEL_NAME = "channel_name"
+const val CHANNEL_ID = "channel_id"
+private const val CHANNEL_DESCRIPTION = "This channel sends the same notification every minute"
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +24,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        createNotificationChannel()
 
         binding.startButton.setOnClickListener { start() }
         binding.stopButton.setOnClickListener { WorkManager.getInstance(applicationContext).cancelAllWork() }
@@ -23,5 +33,15 @@ class MainActivity : AppCompatActivity() {
     private fun start() {
         val workRequest = PeriodicWorkRequestBuilder<ShowNotificationWorker>(15, TimeUnit.MINUTES).build()
         WorkManager.getInstance(applicationContext).enqueue(workRequest)
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH).apply {
+                description = CHANNEL_DESCRIPTION
+            }
+            val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
--- app/src/main/java/com/example/multithreading/ShowNotificationWorker.kt
+++ app/src/main/java/com/example/multithreading/ShowNotificationWorker.kt
@@ -1,21 +1,29 @@
 package com.example.multithreading
 
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.util.Log
-import android.widget.Toast
-import androidx.work.*
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.Worker
+import androidx.work.WorkerParameters
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.util.concurrent.TimeUnit
 
 
+private const val NOTIFICATION_ID = 0
+
 class ShowNotificationWorker(private val context: Context, params: WorkerParameters) :
     Worker(context, params) {
     override fun doWork(): Result {
         repeat(15) {
             val oneNotificationWorker = OneTimeWorkRequestBuilder<OneNotificationWorker>()
-                    .setInitialDelay( it.toLong(), TimeUnit.MINUTES)
+                .setInitialDelay(it.toLong(), TimeUnit.MINUTES)
                 .build()
             WorkManager.getInstance(context).enqueue(oneNotificationWorker)
         }
@@ -29,11 +37,25 @@ class OneNotificationWorker(private val context: Context, params: WorkerParamete
     Worker(context, params) {
     override fun doWork(): Result {
         CoroutineScope(Dispatchers.Main).launch {
-            Toast.makeText(context, "Notificaation!!!", Toast.LENGTH_SHORT).show()
+            val intent = Intent(context, MainActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
+            val pendingIntent: PendingIntent =
+                PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
 
+            val notificationBuilder = NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_star)
+                .setContentTitle(context.getString(R.string.notification_title))
+                .setContentText(context.getString(R.string.notification_text))
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+
+            with(NotificationManagerCompat.from(context)) {
+                notify(NOTIFICATION_ID, notificationBuilder.build())
+            }
         }
         Log.d("Worker", "doWork: After Toast")
         return Result.success()
     }
-
 }
